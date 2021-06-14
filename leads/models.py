78704456f0792cb176f21cebd3120eb40cc3cede
Gleaminggorillas <thomas.henebury@googlemail.com>
2021-06-14 15:10:54 +0100

    migrate; in leads/models, create User(AbstracUser)

--- leads/models.py
+++ leads/models.py
@@ -1,9 +1,24 @@
 from django.db import models
+#v useful abstract class for user - always recommended by Django for future use
+from django.contrib.auth.models import AbstractUser
+
+
 
 # Create your models here.
 
+class User(AbstractUser):
+    pass
+
 class Lead(models.Model):
+
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
+    #Foreign Keys are a one to many from the present class to the Foreign Key 
+    #Foreign Key for table must be above ^^^ without string, or as string within THIS dir
+    agent=models.ForeignKey("Agent", on_delete=models.CASCADE) #CASCADE
 
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)

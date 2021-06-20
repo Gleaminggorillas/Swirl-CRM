0fc7d62d294e0aca17e79ad9b2dab72bf59e8479
Gleaminggorillas <thomas.henebury@googlemail.com>
2021-06-20 17:31:23 +0100

    amend second_page.html to include/prettify Lead names within templated for loop

--- leads/views.py
+++ leads/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Lead
 # Create your views here.
 
 def home_page(request):
+    leads = Lead.objects.all()
     context = {
-        "name": "Joe",
-        "age": 35,
+        "leads": leads
     }
     return render(request, 'second_page.html', context)

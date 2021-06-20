c91122c3b9c7fa85b746eacb52e6e180374669c2
Gleaminggorillas <thomas.henebury@googlemail.com>
2021-06-20 17:18:00 +0100

    create and implement views and basic html via both BASE_DIR/templates in root AND leads/templates/leads

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home_page(request):
+    context = {
+        "name": "Joe",
+        "age": 35,
+    }
+    return render(request, 'second_page.html', context)

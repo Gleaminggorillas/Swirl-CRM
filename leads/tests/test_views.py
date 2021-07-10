4ba7964f48491871507f392da77b839a05991066
Gleaminggorillas <thomas.henebury@googlemail.com>
2021-07-10 21:38:36 +0100

    light testing implementation

---
+++ leads/tests/test_views.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from django.shortcuts import reverse
+
+# Create your tests here.
+
+class LandingPageTest(TestCase):
+
+    def test_status_code(self):
+        # TODO a test
+        response = self.client.get(reverse("landing_page"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "landing.html") 
+
+
+
+
+
+
+

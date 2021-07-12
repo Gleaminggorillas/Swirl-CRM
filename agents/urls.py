3072f96644a41f28f749071c8f76668db1380bd4
Gleaminggorillas <thomas.henebury@googlemail.com>
2021-07-12 14:55:16 +0100

    updating navbar; updating agents.

--- agents/urls.py
+++ agents/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import AgentListView
+from .views import AgentListView, AgentCreateView, AgentDetailView
 
 
 app_name = 'agents'
 
 
 urlpatterns = [
-    path ('', AgentListView.as_view(), name='agents'),
+    path ('', AgentListView.as_view(), name='agent-list'),
+    path('create/', AgentCreateView.as_view(), name='agent-create'),
+    path('<int:pk>/', AgentDetailView.as_view(), name='agent-detail')
     ]
 
 

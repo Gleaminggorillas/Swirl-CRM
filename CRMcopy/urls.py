d3966291c765a04a8dfdca8f524d9f7d4544281f
Gleaminggorillas <thomas.henebury@googlemail.com>
2021-07-13 20:06:57 +0100

    Agent invite and  password reset

--- CRMcopy/urls.py
+++ CRMcopy/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (LoginView, LogoutView, PasswordResetCompleteView, PasswordResetView,
+PasswordResetConfirmView, PasswordResetDoneView, PasswordResetCompleteView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,8 +28,11 @@ urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('agents/', include('agents.urls', namespace='agents'))
-
+    path('agents/', include('agents.urls', namespace='agents')),
+    path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
+    path('reset-password-done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name="password-reset-confirm")
 ] #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 

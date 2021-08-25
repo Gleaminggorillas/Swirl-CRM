942c3aea9970c5c3a243fb9c53fb65baae08d2d5
Gleaminggorillas <thomas.henebury@googlemail.com>
2021-08-25 13:57:00 +0100

    deployment-prep before moving to linux OS

--- CRMcopy/settings.py
+++ CRMcopy/settings.py
@@ -11,8 +11,26 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# for local dev, in terminal type 'export READ_DOT_ENV_FILE=True'
+# or in PowerShell '$env:READ_DOT_ENV_FILE = 'True'
+READ_DOT_ENV_FILE = env.bool('READ_DOT_ENV_FILE', default=False)
+if READ_DOT_ENV_FILE:
+    environ.Env.read_env()
 
 
+
+# False if not in os.environ
+DEBUG = env('DEBUG')
+
+# Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
+SECRET_KEY = env('SECRET_KEY')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,10 +39,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-enjrlu+$mbv#bl&3-cs#1qwz@7z*v-o^3z1(mauc9=*-*!%fy-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
